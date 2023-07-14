6268545cc82435c803ab5116743f7d734b3a0370
phil <philjke@mail.ru>
2023-07-14 13:52:35 +0700

    add start command and webhook

---
+++ webhook.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+const botToken = '6335351112:AAGp6GSdfI7h1Nvb_i0fnhjUEQ8TFKAZfJo';
+const webhookUrl = `http://185.18.55.156/webhook`;
+
+// Установка URL Webhook
+axios
+	.post(`https://api.telegram.org/bot${botToken}/setWebhook?url=${webhookUrl}`)
+	.then((response) => {
+		console.log('Webhook установлен:', response.data);
+	})
+	.catch((error) => {
+		console.error('Ошибка установки Webhook:', error);
+	});
